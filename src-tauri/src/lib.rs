4dcfc87bab11f1ae462dc5ceea31b573ed1cea20
Wihyeongsu <wibrother2@gmail.com>
2025-03-20 09:38:50 +0900

    FEAT: tauri list processes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,30 @@
+use serde::{Deserialize, Serialize};
 use std::env;
+use sysinfo::System;
 
 #[tauri::command]
 fn os_name() -> &'static str {
     env::consts::OS
+}
+
+#[derive(Serialize, Deserialize)]
+struct ProcessInfo {
+    id: String,
+    name: String,
+}
+
+#[tauri::command]
+fn list_process() -> Vec<ProcessInfo> {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    sys.processes()
+        .iter()
+        .map(|(id, process)| ProcessInfo {
+            id: id.to_string(),
+            name: process.name().to_string_lossy().into_owned(),
+        })
+        .collect()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
